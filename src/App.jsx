00b991dbd215fb88f69269c38080b707cd6ae2b1
Ebdollah <l201165@lhr.nu.edu.pk>
2024-08-07 12:10:52 +0500

    Removed unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import Card from "./components/Card"
 import Tcard from "./components/Tcard"
-// import UserList from "./components/UserList"
 
 export default function App() {
   return (
     <>
-      {/* <UserList /> */}
-      {/* <Card /> */}
       <Tcard />
     </>
   )
